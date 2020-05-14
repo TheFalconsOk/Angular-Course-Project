52dadc828bf32c8eb56a90ee4251539db7bd90e4
karl <thefalconsok@yahoo.co.uk>
2020-05-14 15:18:15 +0100

    Add shopping list items

--- src/app/shopping/s-list/s-list.component.ts
+++ src/app/shopping/s-list/s-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../../sharedAssets/ingredient.model';
 
 @Component({
   selector: 'app-s-list',
@@ -6,7 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./s-list.component.css']
 })
 export class SListComponent implements OnInit {
-  ingredients = [];
+  ingredients: Ingredient[] = [
+    new Ingredient("Apples", 5),
+    new Ingredient("Tomatoes", 10)];
 
   constructor() { }
 
